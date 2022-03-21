a15bb8065e6b47adb125b9d4de95d55d8bbb4ce0
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-03-21 12:54:34 +0600

    Callback Hell

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ console.log("LINE 1");
 
 getStudent(1104003,(studentName)=>{
   console.log(studentName);
+  getCourses(studentName,(courses)=>{
+  console.log(courses); 
+  getQuizMarks(courses.courses,(marks)=>{
+    console.log(marks);
+  })
+  })
 })
 
 console.log("Line 2");
@@ -12,4 +18,18 @@ function getStudent(id,callback){
     console.log("Featching From Database....");
     callback ({id:id,name:"Sajib Sarker"})
   },2000)
+}
+
+function getCourses(studentName,callback){
+  setTimeout(()=>{
+  console.log("Student courses from database...")
+  callback ({id:studentName.id,name:studentName.name,courses:["Javascript","Python"]}) 
+  },2000)
+}
+
+function getQuizMarks(courses,callback){
+  setTimeout(()=>{
+  console.log("Student Courses Marks From Database..");
+  callback ({[courses[0]]:5,[courses[1]]:4})
+  },3000)
 }
