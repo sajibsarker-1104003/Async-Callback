92c7613e20b0e3372260526dc992f27cc3861d2c
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-05-08 21:03:18 +0600

    Replacing Callbacks as Promises

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 console.log("LINE 1");
 
-getStudent(1104003,(studentName)=>{
-  console.log(studentName);
-  getCourses(studentName,(courses)=>{
-  console.log(courses); 
-  getQuizMarks(courses.courses,(marks)=>{
-    console.log(marks);
-  })
-  })
+getStudent(1104003).then((student)=>{
+  console.log(student)
+  return getCourses(student)
+
+})
+.then((courses)=>{
+  console.log(courses.courses)
+  return getQuizMarks(courses.courses)
+
+})
+.then((marks)=>{
+  console.log(marks)
 })
 
 console.log("Line 2");
 
 
-function getStudent(id,callback){
-  setTimeout(()=>{
-    console.log("Featching From Database....");
-    callback ({id:id,name:"Sajib Sarker"})
-  },2000)
+function getStudent(id){
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      console.log("Featching From Database....");
+      resolve ({id:id,name:"Sajib Sarker"})
+    },2000)
+  })
+ 
 }
 
-function getCourses(studentName,callback){
-  setTimeout(()=>{
-  console.log("Student courses from database...")
-  callback ({id:studentName.id,name:studentName.name,courses:["Javascript","Python"]}) 
-  },2000)
+function getCourses(studentName){
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      console.log("Student courses from database...")
+      resolve ({id:studentName.id,name:studentName.name,courses:["Javascript","Python"]}) 
+      },3000)
+
+  })
+  
 }
 
 function getQuizMarks(courses,callback){
-  setTimeout(()=>{
-  console.log("Student Courses Marks From Database..");
-  callback ({[courses[0]]:5,[courses[1]]:4})
-  },3000)
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      console.log("Student Courses Marks From Database..");
+      resolve ({[courses[0]]:5,[courses[1]]:4})
+      },4000)
+
+  })
+  
 }
