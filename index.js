7a4c3d5d61d1b54454895cedcef39f571f089474
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-05-08 21:22:54 +0600

    Async and Await example

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+//Promises//
+
 console.log("LINE 1");
 
-getStudent(1104003).then((student)=>{
-  console.log(student)
-  return getCourses(student)
+// getStudent(1104003).then((student)=>{
+//   console.log(student)
+//   return getCourses(student)
 
-})
-.then((courses)=>{
-  console.log(courses.courses)
-  return getQuizMarks(courses.courses)
+// })
+// .then((courses)=>{
+//   console.log(courses.courses)
+//   return getQuizMarks(courses.courses)
 
-})
-.then((marks)=>{
-  console.log(marks)
-})
+// })
+// .then((marks)=>{
+//   console.log(marks)
+// })
+
+//Async and Await
+
+
+async function studentData(){
+  const student=await getStudent(1104003);
+  const courses=await getCourses(student);
+  const marks= await getQuizMarks(courses.courses);
+
+  console.log(marks);
+}
+
+studentData();
 
 console.log("Line 2");
 
@@ -21,7 +36,9 @@ function getStudent(id){
   return new Promise((resolve,reject)=>{
     setTimeout(()=>{
       console.log("Featching From Database....");
-      resolve ({id:id,name:"Sajib Sarker"})
+      const student={id:id,name:"Sajib Sarker"}
+      console.log(student)
+      resolve (student)
     },2000)
   })
  
@@ -31,7 +48,9 @@ function getCourses(studentName){
   return new Promise((resolve,reject)=>{
     setTimeout(()=>{
       console.log("Student courses from database...")
-      resolve ({id:studentName.id,name:studentName.name,courses:["Javascript","Python"]}) 
+      const courses={id:studentName.id,name:studentName.name,courses:["Javascript","Python"]}
+      console.log(courses)
+      resolve (courses) 
       },3000)
 
   })
@@ -42,7 +61,9 @@ function getQuizMarks(courses,callback){
   return new Promise((resolve,reject)=>{
     setTimeout(()=>{
       console.log("Student Courses Marks From Database..");
-      resolve ({[courses[0]]:5,[courses[1]]:4})
+      const marks={[courses[0]]:5,[courses[1]]:4}
+      console.log(marks)
+      resolve (marks)
       },4000)
 
   })
